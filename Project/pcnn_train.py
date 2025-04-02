c10f2662162d821bf35011f8bccec773e27d9e10
Ingrid Kristine Bøe <156179756+IngriKBo@users.noreply.github.com>
2025-04-01 20:00:05 -0700

    add label

--- Project/pcnn_train.py
+++ Project/pcnn_train.py
@@ -25,7 +25,7 @@ def train_or_test(model, data_loader, optimizer, loss_op, device, args, epoch, m
     
     for batch_idx, item in enumerate(tqdm(data_loader)):
         
-        model_input, _ = item # fjerna label
+        model_input, label = item # fjerna label
         label = label.to(device)
         model_input = model_input.to(device)
         model_output = model(model_input, label=label)  ### ensure PixelCNN model recives label tensor
